6ce629b23342cd7223423b8acf72f7cecef77974
boistei <29461756+boistei@users.noreply.github.com>
2023-02-21 08:02:48 +0530

    Refactor code with separate game() function

--- script.js
+++ script.js
@@ -1,42 +1,50 @@
-//Create variables for selection
 let selection = ["Rock","Paper","Scissors"];
 let compScore = 0, playScore = 0;
-//Create a function for computer's selection
-function computerSelection() {
+
+function select() {
     return Math.floor(Math.random()*3);
 }
-//Create a function for player's selection
-function playerSelection() {
-    return Math.floor(Math.random()*3);
+
+function playRound (playerSelection, computerSelection)
+{
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if ((computerSelection === "rock" && playerSelection === "scissors")
+    ||(computerSelection === "paper" && playerSelection === "rock")
+    ||(computerSelection === "scissors" && playerSelection === "paper"))
+    {
+        return "Computer";
+    }
+    else if ((computerSelection === "scissors" && playerSelection === "rock")
+    ||(computerSelection === "rock" && playerSelection === "paper")
+    ||(computerSelection === "paper" && playerSelection === "scissors"))
+    {
+        return "Player";
+    }
+    else if (computerSelection === playerSelection)
+    {
+        return "Tie";
+    }
+    else return "Invalid!";
 }
-//Assign them to variables
-while(compScore < 5 && playScore < 5){
-    let comp = selection[computerSelection()];
-    let play = selection[playerSelection()];
-    //Check if both are working fine.
-    console.log(`The computer chose ${comp} and the player chose ${play}`);
-    //Scenarios where computer wins
-    if ((comp === "Rock" && play === "Scissors")
-    ||(comp === "Paper" && play === "Rock")
-    ||(comp === "Scissors" && play === "Paper"))
-    {
-        compScore++;
-        console.log(`As computer picked ${comp} and player picked ${play}, computer wins!`);
-        console.log(`Computer: ${compScore}; Player: ${playScore}`);
-    }
-    //Scenarios where player wins
-    else if ((comp === "Scissors" && play === "Rock")
-    ||(comp === "Rock" && play === "Paper")
-    ||(comp === "Paper" && play === "Scissors"))
-    {
-        playScore++;
-        console.log(`As computer picked ${comp} and player picked ${play}, player wins!`);
-        console.log(`Computer: ${compScore}; Player: ${playScore}`);
-    }
-    //Scenarios where it is a tie
-    else if (play === comp)
-    {
-        console.log(`It's a tie!`);
-        console.log(`Computer: ${compScore}; Player: ${playScore}`);
-    }
+
+function game () {
+    while(compScore < 5 && playScore < 5) {
+    let comp = selection[select()];
+    let play = prompt("Enter Rock/Paper/Scissors", "Rock");
+    let winner = playRound(play,comp);
+        if (winner === "Computer") {
+            compScore++;
+            console.log(`${comp} beats ${play}. Computer wins this round. Computer: ${compScore}; Player: ${playScore}`);
+        } else if (winner === "Player") {
+            playScore++;
+            console.log(`${play} beats ${comp}. Player wins this round. Computer: ${compScore}; Player: ${playScore}`);
+        } else if (winner === "Tie") {
+            console.log(`It's a tie! Computer: ${compScore}; Player: ${playScore}`);
+        } else {
+            console.log("Invalid selection. Try again!");
+        }
+    }    
 }
+
+game();
